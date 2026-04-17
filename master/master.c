ef52e45894aee4382c19df522120c55bc3cc49e1
Luca Toniolo <toniolo.luca@gmail.com>
2026-04-17 18:50:16 +0800

    master/master: include sched/types.h for sched_param on >= 4.11
    struct sched_param moved from <linux/sched.h> to
    <uapi/linux/sched/types.h> in kernel 4.11. Without the explicit
    include on newer kernels the struct is incomplete and ec_master_eoe_start
    fails to compile.

--- master/master.c
+++ master/master.c
@@ -44,6 +44,9 @@
 #include <linux/device.h>
 #include <linux/version.h>
 #include <linux/hrtimer.h>
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0)
+#include <uapi/linux/sched/types.h>
+#endif
 #include "globals.h"
 #include "slave.h"
 #include "slave_config.h"
